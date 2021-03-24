0a5577b2e2e20cd7529aa63d28aaaf11fb86bd2a
Jordan Reger <jordanreger@gmail.com>
2021-03-24 19:51:42 -0400

    added more

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function Friends() {
       <div className="container-left">
         <div className="container-left-friends">
           <svg
-            class="linkButtonIcon-Mlm5d6"
+            className="linkButtonIcon-Mlm5d6"
             aria-hidden="false"
             width="25"
             height="25"
@@ -60,7 +60,7 @@ function Friends() {
         </div>
         <div className="container-left-nitro">
           <svg
-            class="linkButtonIcon-Mlm5d6"
+            className="linkButtonIcon-Mlm5d6"
             aria-hidden="false"
             width="25"
             height="25"
@@ -84,7 +84,199 @@ function Friends() {
           <div className="directmessages-jordanreger-text">jordanreger</div>
         </div>
       </div>
-      <div className="top-container-right" />
+      <div className="top-container-right">
+        <svg
+          x="0"
+          y="0"
+          className="at-icon-top-container-right"
+          aria-hidden="false"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+        >
+          <path
+            fill="#72767d"
+            d="M12 2C6.486 2 2 6.486 2 12C2 17.515 6.486 22 12 22C14.039 22 15.993 21.398 17.652 20.259L16.521 18.611C15.195 19.519 13.633 20 12 20C7.589 20 4 16.411 4 12C4 7.589 7.589 4 12 4C16.411 4 20 7.589 20 12V12.782C20 14.17 19.402 15 18.4 15L18.398 15.018C18.338 15.005 18.273 15 18.209 15H18C17.437 15 16.6 14.182 16.6 13.631V12C16.6 9.464 14.537 7.4 12 7.4C9.463 7.4 7.4 9.463 7.4 12C7.4 14.537 9.463 16.6 12 16.6C13.234 16.6 14.35 16.106 15.177 15.313C15.826 16.269 16.93 17 18 17L18.002 16.981C18.064 16.994 18.129 17 18.195 17H18.4C20.552 17 22 15.306 22 12.782V12C22 6.486 17.514 2 12 2ZM12 14.599C10.566 14.599 9.4 13.433 9.4 11.999C9.4 10.565 10.566 9.399 12 9.399C13.434 9.399 14.6 10.565 14.6 11.999C14.6 13.433 13.434 14.599 12 14.599Z"
+          ></path>
+        </svg>
+        <span className="username-top-container-right">jordanreger</span>
+        <div className="status" />
+        <div className="voicecallicon">
+          <svg
+            x="0"
+            y="0"
+            className="icon-22AiRD"
+            aria-hidden="false"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="#dcddde"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M11 5V3C16.515 3 21 7.486 21 13H19C19 8.589 15.411 5 11 5ZM17 13H15C15 10.795 13.206 9 11 9V7C14.309 7 17 9.691 17 13ZM11 11V13H13C13 11.896 12.105 11 11 11ZM14 16H18C18.553 16 19 16.447 19 17V21C19 21.553 18.553 22 18 22H13C6.925 22 2 17.075 2 11V6C2 5.447 2.448 5 3 5H7C7.553 5 8 5.447 8 6V10C8 10.553 7.553 11 7 11H6C6.063 14.938 9 18 13 18V17C13 16.447 13.447 16 14 16Z"
+            ></path>
+          </svg>
+        </div>
+        <div className="videocallicon">
+          <svg
+            x="0"
+            y="0"
+            className="icon-22AiRD"
+            aria-hidden="false"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="#dcddde"
+              d="M21.526 8.149C21.231 7.966 20.862 7.951 20.553 8.105L18 9.382V7C18 5.897 17.103 5 16 5H4C2.897 5 2 5.897 2 7V17C2 18.104 2.897 19 4 19H16C17.103 19 18 18.104 18 17V14.618L20.553 15.894C20.694 15.965 20.847 16 21 16C21.183 16 21.365 15.949 21.526 15.851C21.82 15.668 22 15.347 22 15V9C22 8.653 21.82 8.332 21.526 8.149Z"
+            ></path>
+          </svg>
+        </div>
+        <div className="pinnedmessagesicon">
+          <svg
+            x="0"
+            y="0"
+            className="icon-22AiRD"
+            aria-hidden="false"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="#dcddde"
+              d="M22 12L12.101 2.10101L10.686 3.51401L12.101 4.92901L7.15096 9.87801V9.88001L5.73596 8.46501L4.32196 9.88001L8.56496 14.122L2.90796 19.778L4.32196 21.192L9.97896 15.536L14.222 19.778L15.636 18.364L14.222 16.95L19.171 12H19.172L20.586 13.414L22 12Z"
+            ></path>
+          </svg>
+        </div>
+        <div className="addfriendsicon">
+          <svg
+            x="0"
+            y="0"
+            className="icon-22AiRD"
+            aria-hidden="false"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="#dcddde"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M21 3H24V5H21V8H19V5H16V3H19V0H21V3ZM10 12C12.205 12 14 10.205 14 8C14 5.795 12.205 4 10 4C7.795 4 6 5.795 6 8C6 10.205 7.795 12 10 12ZM10 13C5.289 13 2 15.467 2 19V20H18V19C18 15.467 14.711 13 10 13Z"
+            ></path>
+          </svg>
+        </div>
+        <div className="top-container-right-input">
+          <input type="text" placeholder="Search" />
+          <svg
+            className="search-icon"
+            aria-hidden="false"
+            width="15"
+            height="15"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="#72767d"
+              d="M21.707 20.293L16.314 14.9C17.403 13.504 18 11.799 18 10C18 7.863 17.167 5.854 15.656 4.344C14.146 2.832 12.137 2 10 2C7.863 2 5.854 2.832 4.344 4.344C2.833 5.854 2 7.863 2 10C2 12.137 2.833 14.146 4.344 15.656C5.854 17.168 7.863 18 10 18C11.799 18 13.504 17.404 14.9 16.314L20.293 21.706L21.707 20.293ZM10 16C8.397 16 6.891 15.376 5.758 14.243C4.624 13.11 4 11.603 4 10C4 8.398 4.624 6.891 5.758 5.758C6.891 4.624 8.397 4 10 4C11.603 4 13.109 4.624 14.242 5.758C15.376 6.891 16 8.398 16 10C16 11.603 15.376 13.11 14.242 14.243C13.109 15.376 11.603 16 10 16Z"
+            ></path>
+          </svg>
+        </div>
+        <div className="inboxicon">
+          <svg
+            x="0"
+            y="0"
+            className="icon-22AiRD"
+            aria-hidden="false"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M19 3H4.99C3.88 3 3.01 3.89 3.01 5L3 19C3 20.1 3.88 21 4.99 21H19C20.1 21 21 20.1 21 19V5C21 3.89 20.1 3 19 3ZM19 15H15C15 16.66 13.65 18 12 18C10.35 18 9 16.66 9 15H4.99V5H19V15Z"
+              fill="#dcddde"
+            ></path>
+          </svg>
+        </div>
+        <div className="helpicon">
+          <a href="https://majel.me/">
+            <svg
+              x="0"
+              y="0"
+              className="icon-22AiRD"
+              aria-hidden="false"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="#dcddde"
+                d="M12 2C6.486 2 2 6.487 2 12C2 17.515 6.486 22 12 22C17.514 22 22 17.515 22 12C22 6.487 17.514 2 12 2ZM12 18.25C11.31 18.25 10.75 17.691 10.75 17C10.75 16.31 11.31 15.75 12 15.75C12.69 15.75 13.25 16.31 13.25 17C13.25 17.691 12.69 18.25 12 18.25ZM13 13.875V15H11V12H12C13.104 12 14 11.103 14 10C14 8.896 13.104 8 12 8C10.896 8 10 8.896 10 10H8C8 7.795 9.795 6 12 6C14.205 6 16 7.795 16 10C16 11.861 14.723 13.429 13 13.875Z"
+              ></path>
+            </svg>
+          </a>
+        </div>
+        <div className="bottom-box">
+          <img
+            src="https://better-default-discord.netlify.app/Icons/Rainbow-Gradient.gif"
+            alt="pfp"
+            className="bottom-box-pfp"
+          />
+          <span className="bottom-box-username">internet</span>
+          <span className="bottom-box-discriminator">#42069</span>
+          <div className="bottom-box-mic">
+            <svg aria-hidden="false" width="20" height="20" viewBox="0 0 24 24">
+              <path
+                d="M6.7 11H5C5 12.19 5.34 13.3 5.9 14.28L7.13 13.05C6.86 12.43 6.7 11.74 6.7 11Z"
+                fill="#dcddde"
+              ></path>
+              <path
+                d="M9.01 11.085C9.015 11.1125 9.02 11.14 9.02 11.17L15 5.18V5C15 3.34 13.66 2 12 2C10.34 2 9 3.34 9 5V11C9 11.03 9.005 11.0575 9.01 11.085Z"
+                fill="#dcddde"
+              ></path>
+              <path
+                d="M11.7237 16.0927L10.9632 16.8531L10.2533 17.5688C10.4978 17.633 10.747 17.6839 11 17.72V22H13V17.72C16.28 17.23 19 14.41 19 11H17.3C17.3 14 14.76 16.1 12 16.1C11.9076 16.1 11.8155 16.0975 11.7237 16.0927Z"
+                fill="#dcddde"
+              ></path>
+              <path
+                d="M21 4.27L19.73 3L3 19.73L4.27 21L8.46 16.82L9.69 15.58L11.35 13.92L14.99 10.28L21 4.27Z"
+                class="strikethrough-1n4ekb"
+                fill="#f04747"
+              ></path>
+            </svg>
+          </div>
+          <div className="bottom-box-headphones">
+            <svg aria-hidden="false" width="20" height="20" viewBox="0 0 24 24">
+              <svg width="24" height="24" viewBox="0 0 24 24">
+                <path
+                  d="M12 2.00305C6.486 2.00305 2 6.48805 2 12.0031V20.0031C2 21.1071 2.895 22.0031 4 22.0031H6C7.104 22.0031 8 21.1071 8 20.0031V17.0031C8 15.8991 7.104 15.0031 6 15.0031H4V12.0031C4 7.59105 7.589 4.00305 12 4.00305C16.411 4.00305 20 7.59105 20 12.0031V15.0031H18C16.896 15.0031 16 15.8991 16 17.0031V20.0031C16 21.1071 16.896 22.0031 18 22.0031H20C21.104 22.0031 22 21.1071 22 20.0031V12.0031C22 6.48805 17.514 2.00305 12 2.00305Z"
+                  fill="#dcddde"
+                ></path>
+              </svg>
+            </svg>
+          </div>
+          <div className="bottom-box-settings">
+            <svg aria-hidden="false" width="20" height="20" viewBox="0 0 24 24">
+              <path
+                fill="#dcddde"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M19.738 10H22V14H19.739C19.498 14.931 19.1 15.798 18.565 16.564L20 18L18 20L16.565 18.564C15.797 19.099 14.932 19.498 14 19.738V22H10V19.738C9.069 19.498 8.203 19.099 7.436 18.564L6 20L4 18L5.436 16.564C4.901 15.799 4.502 14.932 4.262 14H2V10H4.262C4.502 9.068 4.9 8.202 5.436 7.436L4 6L6 4L7.436 5.436C8.202 4.9 9.068 4.502 10 4.262V2H14V4.261C14.932 4.502 15.797 4.9 16.565 5.435L18 3.999L20 5.999L18.564 7.436C19.099 8.202 19.498 9.069 19.738 10ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"
+              ></path>
+            </svg>
+          </div>
+        </div>
+        <div className="main-text-input">
+          <input
+            type="text"
+            placeholder="Message @jordanreger"
+            id="main-text-input"
+          />
+        </div>
+      </div>
       <Link to="/channels/@me">
         <div className="friends-icon-friends">
           <svg
